69aeeccc54f340c2205195372224efdb5763ebc3
Vasu Sahu <160463336+VasuS609@users.noreply.github.com>
2025-11-16 18:40:36 +0530

    Update vite.config.js

--- studybuddy-frontend/vite.config.js
+++ studybuddy-frontend/vite.config.js
@@ -6,7 +6,6 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -18,7 +17,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: process.env.VITE_API_BASE_URL,
         changeOrigin: true,
         rewrite: (path) => path,
       },
