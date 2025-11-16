98180e2b58e45f34839c9408cb3343c80645e668
Vasu Sahu <160463336+VasuS609@users.noreply.github.com>
2025-11-16 18:56:38 +0530

    Update api.js

--- studybuddy-frontend/src/api.js
+++ studybuddy-frontend/src/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // CHANGE THIS LINE
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
 });
+
 // Chat
 export const askChatbot = (question) =>
   API.post("/chat", { question, userId: "guest" });
